651804a2fbf25b0c3005355eb8759d3719f6e493
Luiz Futema <futemaaaa@gmail.com>
2024-08-23 14:22:00 -0300

    teste de redirecionamento de páginas

--- testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
@@ -1,11 +1,31 @@
 describe('Testando múltiplas páginas', () => {
-    it('Deve conseguir acessar a página de cartões', () => {
-        cy.visit('/')
-        cy.getByData('botao-login').click()
-        cy.getByData('email-input').type('neilton@alura.com')
-        cy.getByData('senha-input').type('123456')
-        cy.getByData('botao-enviar').click()
-        cy.getByData('app-home').find('a').eq(1).click()
-        cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
+    beforeEach('Deve conseguir acessar a página de cartões', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura.com')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+
     })
-})
+
+
+    it('Deve conseguir acessar a página de cartões', () => {
+
+    cy.getByData('app-home').find('a').eq(1).click()
+    cy.location('pathname').should('eq','/home/cartoes')
+    cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
+    })
+
+    it('Deve conseguir acessar a página de investimentos', () => {
+
+    cy.getByData('app-home').find('a').eq(3).click()
+    cy.location('pathname').should('eq','/home/investimentos')
+    })
+
+    it('Deve conseguir acessar a página de serviços', () => {
+
+    cy.getByData('app-home').find('a').eq(2).click()
+    cy.location('pathname').should('eq','/home/servicos')
+    })
+
+    })
