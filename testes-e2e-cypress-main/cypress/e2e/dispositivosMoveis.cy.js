4c77394d50494cb1372e1366cc51f7c0e67e9569
Luiz Futema <futemaaaa@gmail.com>
2024-08-29 10:57:38 -0300

    Teste de implementação de vizualização no mobile na horizontal

--- testes-e2e-cypress-main/cypress/e2e/dispositivosMoveis.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/dispositivosMoveis.cy.js
@@ -12,6 +12,28 @@ describe('Testando dispositivos móveis', () => {
         cy.getByData('menu-burguer').click()
         cy.getByData('menu-lateral').find('a').eq(3).click()			
 
-        cy.location('pathname').should('eq','/home/investimentos')			
+        cy.location('pathname').should('eq','/home/investimentos')		
+        
+        cy.viewport(550, 750)
+		cy.viewport('iphone-6', 'landscape')
+    })
+
+        context('Resolução do iphone-5 ', () => {
+          beforeEach(() => {
+            /* roda os testes como se fossem em um dispositivo com a resolução de um iphone-5 */
+            cy.viewport('iphone-5')
+          })
+
+          /* seu teste aqui */
+
+          describe('Menu de navegação', () => {
+            context('Resolução de 720p', () => {
+              beforeEach(() => {
+                /* Roda os testes como se fossem em um monitor de 720p de resolução */
+                cy.viewport(1280, 720)
+              })
+              
+            })
+        })
     })
 })
